bfcbd94616d2a3f29ef9bdd6a01bf82b4e1d644b
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-05-03 14:21:13 +0200

    Use tokio for asynchronous gstreamer worker.

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,98 @@
-use gstreamer::ffi::GstValueList;
-use gstreamer::{self as gst, MessageType};
 use gstreamer::prelude::*;
+use gstreamer::{self as gst};
 
-fn main() {
-    // 1. Inicjalizacja GStreamera
-    gst::init().expect("Nie udało się zainicjować GStreamera");
+fn stream(
+    sender: tokio::sync::watch::Sender<Vec<f32>>,
+    cancel_token: tokio_util::sync::CancellationToken,
+) -> tokio::task::JoinHandle<()> {
+    tokio::task::spawn_blocking(move || {
+        gst::init().expect("initialization failed");
 
-    // 2. Budowa potoku (pipeline) z wtyczką spectrum
-    let pipeline_str = "
-        udpsrc port=5001 caps=\"audio/x-raw,rate=44100,channels=2,format=S16LE\" !
-        queue ! audioconvert ! audioresample !
-        spectrum name=spec bands=64 ! fakesink
-    ";
+        let pipeline_str = "
+            udpsrc port=5001 caps=\"audio/x-raw,rate=44100,channels=2,format=S16LE\" !
+            queue ! audioconvert ! audioresample !
+            spectrum name=spec bands=64 ! fakesink
+        ";
 
-    let pipeline = gst::parse::launch(pipeline_str)
-        .expect("Błąd podczas tworzenia potoku")
-        .dynamic_cast::<gst::Pipeline>()
-        .expect("To nie jest prawidłowy potok");
+        let pipeline = gst::parse::launch(pipeline_str).expect("pipeline creation failed");
 
-    // Uruchamiamy odtwarzanie potoku
-    pipeline
-        .set_state(gst::State::Playing)
-        .expect("Nie udało się uruchomić potoku");
+        pipeline
+            .set_state(gst::State::Playing)
+            .expect("unable to set the pipeline to the `Playing` state");
 
-    println!("GStreamer działa i nasłuchuje...");
+        let bus = pipeline.bus().unwrap();
 
-    // Wait until error or EOS
-    let bus = pipeline.bus().unwrap();
-    for msg in bus.iter_timed_filtered(
-        gst::ClockTime::NONE,
-        &[MessageType::Error, MessageType::Eos, MessageType::Element],
-    ) {
-        use gst::MessageView;
-
-        match msg.view() {
-            MessageView::Element(element_msg) => {
-                // Sprawdzamy, czy to wiadomość od naszej wtyczki 'spectrum'
-                if let Some(s) = element_msg.structure() {
-                    if s.name() == "spectrum" {
-                        // Wyciągamy tablicę wartości w decybelach (magnitude)
-                        if let Ok(magnitudes) = s.get::<gst::List>("magnitude") {
-                            // 'values' to wektor 64 liczb typu float
-                            // TODO: Przekaż te dane do kanału (np. tokio::sync::broadcast)
-                            // aby Axum wysłał je przez SSE do przeglądarek.
-                            println!("Odebrano pasma FFT: {:?}", magnitudes);
-                        }
-                    }
-                }
-            }
-            MessageView::Eos(..) => break,
-            MessageView::Error(err) => {
-                println!(
-                    "Error from {:?}: {} ({:?})",
-                    err.src().map(|s| s.path_string()),
-                    err.error(),
-                    err.debug()
-                );
+        loop {
+            if cancel_token.is_cancelled() {
                 break;
             }
-            _ => (),
+
+            let msg = bus.timed_pop_filtered(
+                gst::ClockTime::from_mseconds(100),
+                &[
+                    gst::MessageType::Error,
+                    gst::MessageType::Eos,
+                    gst::MessageType::Element,
+                ],
+            );
+
+            let Some(msg) = msg else {
+                continue;
+            };
+
+            use gst::MessageView;
+
+            match msg.view() {
+                MessageView::Element(element_msg) => {
+                    if let Some(magnitudes) = element_msg
+                        .structure()
+                        .filter(|s| s.name() == "spectrum")
+                        .and_then(|s| s.get::<gst::List>("magnitude").ok())
+                    {
+                        let magnitudes: Vec<f32> = magnitudes
+                            .as_slice()
+                            .iter()
+                            .map(|v| v.get::<f32>().unwrap())
+                            .collect();
+                        let _ = sender.send(magnitudes);
+                    }
+                }
+                MessageView::Eos(..) => break,
+                MessageView::Error(err) => {
+                    println!(
+                        "Error from {:?}: {} ({:?})",
+                        err.src().map(|s| s.path_string()),
+                        err.error(),
+                        err.debug()
+                    );
+                    break;
+                }
+                _ => (),
+            }
+        }
+        pipeline
+            .set_state(gst::State::Null)
+            .expect("Unable to set the pipeline to the `Null` state");
+    })
+}
+
+#[tokio::main]
+async fn main() {
+    let (tx, mut rx) = tokio::sync::watch::channel(Vec::<f32>::new());
+    let cancel_token = tokio_util::sync::CancellationToken::new();
+    let stream = stream(tx, cancel_token.clone());
+
+    loop {
+        tokio::select! {
+            _ = tokio::signal::ctrl_c() => {
+                cancel_token.cancel();
+                break
+            },
+            _ = tokio::time::sleep(tokio::time::Duration::from_millis(100)) => {
+                println!("Odebrane pasma FFT: {:?}", rx.borrow_and_update());
+            }
         }
     }
 
-    println!("GStreamer kończy działanie");
-
-    // Zatrzymujemy potok przed zamknięciem programu
-    pipeline
-        .set_state(gst::State::Null)
-        .expect("Nie udało się zatrzymać potoku");
+    stream.await.unwrap();
 }
