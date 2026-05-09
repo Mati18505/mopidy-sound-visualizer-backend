20bd2bcf8559f52290f331bae47c3150b3b626de
Mateusz Stulczewski <mstulczewski@gmail.com>
2026-05-09 23:01:52 +0200

    Improve performance by using mpsc and broadcast channels.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
-use std::{convert::Infallible, sync::{Arc, atomic::{AtomicU64, Ordering}}};
+use std::{
+    convert::Infallible,
+    sync::{
+        Arc,
+        atomic::{AtomicU64, Ordering},
+    },
+};
 
 use axum::{
-    Router, extract::State, response::sse::{Event, KeepAlive, Sse}, routing::get
+    Router,
+    extract::State,
+    response::{Sse, sse},
+    routing::get,
 };
 use gstreamer::prelude::*;
 use gstreamer::{self as gst};
+use serde_json::json;
 use tokio_stream::StreamExt;
 
 static GST: AtomicU64 = AtomicU64::new(0);
-static WATCH: AtomicU64 = AtomicU64::new(0);
+static TRANSFORMATIONS: AtomicU64 = AtomicU64::new(0);
 static SSE: AtomicU64 = AtomicU64::new(0);
 
 fn stream(
-    sender: tokio::sync::watch::Sender<Vec<f32>>,
+    sender: tokio::sync::mpsc::Sender<Vec<f32>>,
     cancel_token: tokio_util::sync::CancellationToken,
 ) -> tokio::task::JoinHandle<()> {
     tokio::task::spawn_blocking(move || {
@@ -64,7 +74,7 @@ fn stream(
                             .iter()
                             .map(|v| v.get::<f32>().unwrap())
                             .collect();
-                        let _ = sender.send(magnitudes);
+                        let _ = sender.blocking_send(magnitudes);
 
                         GST.fetch_add(1, Ordering::Relaxed);
                     }
@@ -89,21 +99,34 @@ fn stream(
 }
 
 struct AppState {
-    rx: tokio::sync::watch::Receiver<Vec<f32>>,
+    rx: tokio::sync::broadcast::Receiver<Arc<String>>,
 }
 
-async fn sse_handler(State(state): State<Arc<AppState>>) -> Sse<impl futures_util::Stream<Item = Result<Event, Infallible>>> {
-    let stream = tokio_stream::wrappers::WatchStream::new(state.rx.clone())
-        .map(|data| {
-            SSE.fetch_add(1, Ordering::Relaxed);
-            Ok(Event::default().json_data(data).unwrap())
-        });
+async fn sse_handler(
+    State(state): State<Arc<AppState>>,
+) -> Sse<impl futures_util::Stream<Item = Result<sse::Event, Infallible>>> {
+let stream = tokio_stream::wrappers::BroadcastStream::new(state.rx.resubscribe())
+    .filter_map(|data| {
+        let text = data.ok()?;
 
-    Sse::new(stream).keep_alive(KeepAlive::default())
+        SSE.fetch_add(1, Ordering::Relaxed);
+
+        Some(Ok::<_, Infallible>(
+            sse::Event::default().data(text.to_string()),
+        ))
+    });
+
+    Sse::new(stream).keep_alive(sse::KeepAlive::default())
 }
 
-fn serve_web(shared_state: Arc<AppState>, cancel_token: tokio_util::sync::CancellationToken) -> tokio::task::JoinHandle<()> {
-    let app = Router::<Arc<AppState>>::new().route("/sse", get(sse_handler)).layer(tower_http::cors::CorsLayer::permissive()).with_state(shared_state);
+fn serve_web(
+    shared_state: Arc<AppState>,
+    cancel_token: tokio_util::sync::CancellationToken,
+) -> tokio::task::JoinHandle<()> {
+    let app = Router::<Arc<AppState>>::new()
+        .route("/sse", get(sse_handler))
+        .layer(tower_http::cors::CorsLayer::permissive())
+        .with_state(shared_state);
 
     tokio::spawn(async move {
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -115,13 +138,27 @@ fn serve_web(shared_state: Arc<AppState>, cancel_token: tokio_util::sync::Cancel
     })
 }
 
+fn transform_data(mut gst_in: tokio::sync::mpsc::Receiver<Vec<f32>>, str_out: tokio::sync::broadcast::Sender<Arc<String>>) -> tokio::task::JoinHandle<()>  {
+    tokio::spawn(async move {
+        while let Some(data) = gst_in.recv().await {
+            TRANSFORMATIONS.fetch_add(1, Ordering::Relaxed);
+            let text = json!(data).to_string();
+            let _ = str_out.send(Arc::new(text));
+        };
+    })
+}
+
 #[tokio::main]
 async fn main() {
-    let (tx, mut rx) = tokio::sync::watch::channel(Vec::<f32>::new());
     let cancel_token = tokio_util::sync::CancellationToken::new();
-    let stream_handle = stream(tx, cancel_token.clone());
 
-    let shared_state = Arc::new(AppState{rx: rx.clone()});
+    let (gst_in, gst_out) = tokio::sync::mpsc::channel(128);
+    let (web_in, web_out) = tokio::sync::broadcast::channel(128);
+
+    let stream_handle = stream(gst_in, cancel_token.clone());
+    let transformer = transform_data(gst_out, web_in);
+
+    let shared_state = Arc::new(AppState { rx: web_out });
     let web_server_handle = serve_web(shared_state, cancel_token.clone());
 
     let mut interval = tokio::time::interval(tokio::time::Duration::from_secs(1));
@@ -132,14 +169,11 @@ async fn main() {
                 cancel_token.cancel();
                 break
             },
-            _ = rx.changed() => {
-                WATCH.fetch_add(1, Ordering::Relaxed);
-            },
             _ = interval.tick() => {
                 println!(
-                    "gst: {}/s, watch: {}/s, sse: {}/s",
+                    "gst: {}/s, transformations: {}/s, sse: {}/s",
                     GST.swap(0, Ordering::Relaxed),
-                    WATCH.swap(0, Ordering::Relaxed),
+                    TRANSFORMATIONS.swap(0, Ordering::Relaxed),
                     SSE.swap(0, Ordering::Relaxed),
                 );
             },
@@ -147,5 +181,6 @@ async fn main() {
     }
 
     stream_handle.await.unwrap();
+    transformer.await;
     web_server_handle.await.unwrap();
 }
